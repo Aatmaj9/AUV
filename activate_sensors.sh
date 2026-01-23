dff122bdfe82d4164cf6af08f7626ea3b72e0a6e
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-01-23 21:48:28 +0530

    fixed alias for sonars

--- activate_sensors.sh
+++ activate_sensors.sh
@@ -1,5 +1,11 @@
-source udev.sh
-#Please note the order of sensors - ping360 then ping2 then sbg then zed
-docker exec -d auv_dev bash -ic 'source ~/.bashrc; dvl & ping360 & ping2 & sbg & zed &'
-docker exec -it auv_dev bash
-echo "Sensors started"
+#!/usr/bin/env bash
+set -m  
+trap "kill 0" SIGTERM SIGINT
+
+dvl &
+ping360 &
+ping2 &
+sbg &
+zed &
+
+wait
--- deactivate_sensors.sh
+++ deactivate_sensors.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-docker exec auv_dev bash -c "
-pkill -f ping2
-pkill -f ping360
-pkill -f dvl
-pkill -f sbg
-pkill -f zed
-"
-echo "Sensors stopped"
+pkill -f activate_sensors.sh
