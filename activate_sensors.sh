f1578e5cd057c96bf04881ec67e439da1e3abb94
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-01-23 22:11:25 +0530

    fixing activate sensors script

--- activate_sensors.sh
+++ activate_sensors.sh
@@ -1,5 +1,4 @@
 source udev.sh
-docker exec -d auv_dev bash -ic 'source ~/.bashrc; dvl & ping360 & sleep 4; ping2 & sleep 4; sbg & sleep 2; zed &'
-
+docker exec -d auv_dev bash -ic 'source ~/.bashrc; dvl & ping360; sleep 6; ping2; sleep 4; sbg & zed &'
 echo "Sensors started"
 docker exec -it auv_dev bash
