4c9824fcf4c3ae8757b327d168c126006150a5a5
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-01-23 22:06:25 +0530

    fixing activate sensors script

--- deactivate_sensors.sh
+++ deactivate_sensors.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 docker exec auv_dev bash -c '
-pkill -9 -f "ros2 run ping360_sonar"
-pkill -9 -f "ros2 run ping_sonar_ros"
-pkill -9 -f "ros2 launch sbg_driver"
-pkill -9 -f "ros2 launch zed_wrapper"
-pkill -9 -f "ros2 launch dvl_a50"
+pgrep -f "ros2 run ping360_sonar" | xargs -r kill -9
+pgrep -f "ros2 run ping_sonar_ros" | xargs -r kill -9
+pgrep -f "ros2 launch sbg_driver" | xargs -r kill -9
+pgrep -f "ros2 launch zed_wrapper" | xargs -r kill -9
+pgrep -f "ros2 launch dvl_a50" | xargs -r kill -9
 '
 
 echo "Sensors stopped"
