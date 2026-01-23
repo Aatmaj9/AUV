ab7dc77b3d1106b8f4e7a9b725f66cc89a391d9c
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-01-23 19:12:27 +0530

    order_matters

--- activate_sensors.sh
+++ activate_sensors.sh
@@ -1,4 +1,5 @@
 source udev.sh
-docker exec -d auv_dev bash -ic 'source ~/.bashrc; dvl & ping2 & ping360 & sbg & zed &'
+#Please note the order of sensors - ping360 then ping2 then sbg then zed
+docker exec -d auv_dev bash -ic 'source ~/.bashrc; dvl & ping360 & ping2 & sbg & zed &'
 docker exec -it auv_dev bash
 echo "Sensors started"
