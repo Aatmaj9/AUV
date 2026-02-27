da73e2a14dc06cb34ad680bdce7f1399408e0831
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-02-27 17:34:25 +0530

    trying to fix cuda torch

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 docker buildx build \
   --platform linux/arm64 \
   --pull \
-  --no-cache \
-  -f .devcontainer/Dockerfile_portenta_zedless \
-  -t aatmaj9/auv_portenta_zedless:1.0 \
+  -f .devcontainer/Dockerfile_zedless \
+  -t aatmaj9/auv_zedless:1.0 \
   --push \
   .
