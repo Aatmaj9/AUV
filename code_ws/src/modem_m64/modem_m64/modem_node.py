59d7087d3a34455a7a3cd773ad107e45fa248951
Aatmaj9 <na22b018@smail.iitm.ac.in>
2026-03-09 02:23:28 +0530

    trying to fix ping2

--- code_ws/src/modem_m64/modem_m64/modem_node.py
+++ code_ws/src/modem_m64/modem_m64/modem_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from wlmodem import WlModem 
+from wlmodem import WlModem  # type: ignore
 
 class ModemNode(Node):
     def __init__(self):
